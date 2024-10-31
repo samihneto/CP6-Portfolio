5fb39a0a8c3bfa184cb1769bb81477b9cabe7509
Samir Neto <samihneto@gmail.com>
2024-10-30 23:51:17 -0300

    Mais um ajuste no Readme.
    Co-authored-by: Jennifer Suzuki <jenniesuzuki@users.noreply.github.com>
    Co-authored-by: Felipe_Fidelix <felipe-2833@users.noreply.github.com>
    Co-authored-by: victoraugustogfavaro <victoraugustogfavaro@users.noreply.github.com>

--- cp6portfolio/README.md
+++ cp6portfolio/README.md
@@ -9,6 +9,8 @@ O intuito do projeto é ser uma página de portfólio que recebe e registra as n
 - **Victor Augusto G. Fávaro** - RM: **555059**
 - **Felipe Levi Stephens Fidelix** - RM: **556426**
 
+### Turma 1TDSPG
+
 ## Deploy on Vercel
 
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
--- cp6portfolio/src/app/api/dados.ts
+++ cp6portfolio/src/app/api/dados.ts
@@ -1,7 +1,7 @@
 export let aval = [
     {avalId:1 , alunoNome: "Samir", materia: "Front-End", tipoAval: "Checkpoint", nota: 9},
-    {avalId:2 , alunoNome: "Flavio", materia: "Python", tipoAval: "Checkpoint", nota: 10},
-    {avalId:3 , alunoNome: "João Paulo", materia: "Banco de Dados", tipoAval: "Checkpoint", nota: 5},
+    {avalId:2 , alunoNome: "Victor", materia: "Python", tipoAval: "Checkpoint", nota: 10},
+    {avalId:3 , alunoNome: "Felipe Levy", materia: "Banco de Dados", tipoAval: "Checkpoint", nota: 5},
     {avalId:4 , alunoNome: "Diego", materia: "Python", tipoAval: "Checkpoint", nota: 6},
 
 ]
